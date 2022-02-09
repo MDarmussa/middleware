ce556dfb2e30aaf8f70b460913ad4b7c151ffca9
Mohammad <mhamed.mussa@gmail.com>
2022-02-08 23:38:25 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+ ----- Middleware ------
+setup:
+- npm init
+- npm install express
+- npm i morgan  (https://www.npmjs.com/package/morgan)
+- npm i helmet ---- help to secure express apps by setting various HTTP headers. (https://www.npmjs.com/package/helmet)
+
+
+
+
+----------------------
+Notes:
+* means all
---
+++ app.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const express = require('express');
+const morgan = require('morgan')
+const logger = morgan('tiny') //built in, referes to the status of morgan and how long it took to run
+const helmet = require("helmet");
+const HOST = '127.0.0.1';
+const PORT = 3000;
+
+
+const app = express();
+const server = http.createServer(app);
+
+
+app.use(helmet());
+app.use(logger);
+app.use(express.static('public')); //for attaching images and styles
+
+
+app.use((req, res, next) => {
+     console.log(`Request at ${new Date()}`);
+     next();
+});
+
+// app.use((req, res, next) => {
+//     console.log(`${req.method} ${req.path}`);
+//     next();
+// });
+
+app.get('/', (req, res) => {
+    res.send('Hello Middleware');
+});
+
+server.listen(PORT, HOST, () => {
+    console.log(`Listening on http://${HOST}:${PORT}`);
+});
